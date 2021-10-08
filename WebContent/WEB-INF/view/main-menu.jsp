362021310f381dd54be5511dc1c29e1fc2f5fc7d
AlperAskin <alperaskin@hotmail.com>
2021-10-08 16:25:06 +0300

    Adding data to the model

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -5,6 +5,9 @@
 
 <h2> Hello Spring MVC- Home Page</h2>
 
+<hr>
+
+<a href="showForm">Hello World form</a>
 </body>
 
 </html>
---
+++ src/com/alper/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,42 @@
+package com.alper.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.*;
+
+
+
+@Controller
+public class HelloWorldController {
+	
+	//Controller method to show the initial HTML form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	//Controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	//new controller method to read form data and add data to the model
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		//read the request parameter from html form
+		String theName= request.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Yo!" + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+
+}
